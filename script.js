a4240a045ca39e38d09a98e71ba4e3739f5d4454
Your Name <xodjamovaminjon@gmail.com>
2022-09-27 15:43:56 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// 1 Д/з
+
+let carPrices = [12000, 16000, 30000, 8000, 6000, 9000, 45000, 35000, 70000]
+
+let cheap = []
+if (carPrices.filter(item => console.log(item == 9000))) {
+    cheap.push(carPrices.item)
+    console.log(cheap);
+} else {
+    alert(`ok`)
+}
+
+
+
+
+// 2 Д/з
+
+// let index = +prompt(`Index?`)
+// let arr = ['Aleksey', 'Kartoshka', 'Margarita', 'Morgenshtern', 'Monica']
+
+// if (index === 0) {
+//     alert(`Имя удалено!`)
+//     arr.splice(0, 1)
+//     console.log(arr);
+// } else if (index === 1) {
+//     alert(`Имя удалено!`)
+//     arr.splice(1, 1)
+//     console.log(arr);
+
+// } else if (index === 2) {
+//     alert(`Имя удалено!`)
+//     arr.splice(2, 1)
+//     console.log(arr);
+
+// } else if (index === 3) {
+//     alert(`Имя удалено!`)
+//     arr.splice(3, 1)
+//     console.log(arr);
+
+// } else if (index === 4) {
+//     alert(`Имя удалено!`)
+//     arr.splice(4, 1)
+//     console.log(arr);
+
+// } else { alert(`Такого элемента у вас нет!`) }
+
+
+
+
+//3 Д/з
+// let arr_2 =  [5, 4, 4, 4, 4, 4, false, 'hello', 24, 'world', undefined, null, 'error', 22]
+// if (arr_2.indexOf(Number) === true) {
+//     console.log(`Все хорошо!`);
+
+// } else {
+//     console.log(`Меньше 5 цифр!`);
+// }   
